a1d5efdf630605a6623d96fbebf1b85fc49906d8
François Pellissier <francois.pellissier@gmail.com>
2015-04-16 16:09:37 +0200

    Fix Sending Flurry Events

--- app/src/main/java/social/entourage/android/EntourageApplication.java
+++ app/src/main/java/social/entourage/android/EntourageApplication.java
@@ -2,6 +2,7 @@ package social.entourage.android;
 
 import android.app.Application;
 import android.content.Context;
+import android.util.Log;
 
 import com.flurry.android.FlurryAgent;
 
@@ -16,15 +17,18 @@ public class EntourageApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        //Flurry init
+        FlurryAgent.setLogEnabled(true);
+        FlurryAgent.setLogLevel(Log.VERBOSE);
+        //FlurryAgent.setReportLocation(true);
+        FlurryAgent.setLogEvents(true);
+        FlurryAgent.init(this, BuildConfig.FLURRY_API_KEY);
+        //End of Flurry
 
         JodaTimeAndroid.init(this);
 
         objectGraph = ObjectGraph.create(Modules.list(this));
         inject(this);
-        FlurryAgent.setReportLocation(true);
-        FlurryAgent.setLogEvents(true);
-        FlurryAgent.setLogEnabled(true);
-        FlurryAgent.init(this, BuildConfig.FLURRY_API_KEY);
     }
 
     public void inject(final Object o) {
