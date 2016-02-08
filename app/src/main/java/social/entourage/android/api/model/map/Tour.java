fc45b7fc0c5e8918d55f9c14195d1bfd72fbe39e
OCTO-NTE <ntelera@octo.com>
2016-02-08 16:08:32 +0100

    gps status check updated

--- app/src/main/java/social/entourage/android/api/model/map/Tour.java
+++ app/src/main/java/social/entourage/android/api/model/map/Tour.java
@@ -284,9 +284,13 @@ public class Tour implements Serializable {
     public static class TourComparatorOldToNew implements Comparator<Tour> {
         @Override
         public int compare(Tour tour1, Tour tour2) {
-            Date date1 = tour1.getStartTime();
-            Date date2 = tour2.getStartTime();
-            return date1.compareTo(date2);
+            if (tour1.getStartTime() != null && tour2.getStartTime() != null) {
+                Date date1 = tour1.getStartTime();
+                Date date2 = tour2.getStartTime();
+                return date1.compareTo(date2);
+            } else {
+                return 0;
+            }
         }
     }
 
