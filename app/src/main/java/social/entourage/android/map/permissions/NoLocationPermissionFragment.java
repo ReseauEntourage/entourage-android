7054f13426a006ca29b663cd7ac9cc808316e360
Mihai Ionescu <mihai.ionescu@pontussoftware.ro>
2016-03-07 12:39:20 +0200

    EMA-104 Screen 04.4 Made the dialog as full screen

--- app/src/main/java/social/entourage/android/map/permissions/NoLocationPermissionFragment.java
+++ app/src/main/java/social/entourage/android/map/permissions/NoLocationPermissionFragment.java
@@ -1,6 +1,8 @@
 package social.entourage.android.map.permissions;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
@@ -27,5 +29,13 @@ public class NoLocationPermissionFragment extends DialogFragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_no_location_permission, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        getDialog().getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        getDialog().getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
     }
 }
