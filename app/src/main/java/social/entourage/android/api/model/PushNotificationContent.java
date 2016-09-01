fa9ad077c0bf9d7ec6927d79f90f993fb66cbe18
Mihai Ionescu <mihai.ionescu@pontussoftware.ro>
2016-09-01 15:02:07 +0300

    EMA-567 Finished the invitation push notification

--- app/src/main/java/social/entourage/android/api/model/PushNotificationContent.java
+++ app/src/main/java/social/entourage/android/api/model/PushNotificationContent.java
@@ -12,7 +12,7 @@ public class PushNotificationContent implements Serializable {
     public static final String TYPE_NEW_CHAT_MESSAGE = "NEW_CHAT_MESSAGE";
     public static final String TYPE_JOIN_REQUEST_ACCEPTED = "JOIN_REQUEST_ACCEPTED";
     public static final String TYPE_NEW_JOIN_REQUEST = "NEW_JOIN_REQUEST";
-    public static final String TYPE_ENTOURAGE_INVITATION = "ENTORAUGE_INVITATION";
+    public static final String TYPE_ENTOURAGE_INVITATION = "ENTOURAGE_INVITATION";
 
     public Extra extra;
 
