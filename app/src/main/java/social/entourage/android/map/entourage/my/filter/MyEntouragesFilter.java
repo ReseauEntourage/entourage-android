cddfdbf393eaddb550de6ff6fc329ff04315b63f
Mihai Ionescu <mihai.ionescu@pontussoftware.ro>
2016-10-04 19:00:59 +0300

    EMA-738 Screen17.2 My entourages filter changes

--- app/src/main/java/social/entourage/android/map/entourage/my/filter/MyEntouragesFilter.java
+++ app/src/main/java/social/entourage/android/map/entourage/my/filter/MyEntouragesFilter.java
@@ -10,7 +10,7 @@ public class MyEntouragesFilter {
     public boolean activeEntourages = true;
     public boolean closedEntourages = true;
     public boolean showOwnEntourages = true;
-    public boolean showJoinedEntourages= true;
+    public boolean showJoinedEntourages= false;
 
     public boolean entourageTypeDemand = true;
     public boolean entourageTypeContribution = true;
