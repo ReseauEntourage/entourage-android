60106d1327aa4f3e4b1f1916c6fc2b5c086ad115
Mihai Ionescu <mihai.ionescu@pontussoftware.ro>
2016-10-06 14:28:03 +0300

    EMA-739 Login activity is forced closed if exists and user is logged in

--- app/src/main/java/social/entourage/android/EntourageApplication.java
+++ app/src/main/java/social/entourage/android/EntourageApplication.java
@@ -8,10 +8,13 @@ import com.flurry.android.FlurryAgent;
 
 import net.danlew.android.joda.JodaTimeAndroid;
 
+import java.util.ArrayList;
+
 import social.entourage.android.api.ApiModule;
 import social.entourage.android.api.model.User;
 import social.entourage.android.authentication.AuthenticationController;
 import social.entourage.android.authentication.AuthenticationModule;
+import social.entourage.android.authentication.login.LoginActivity;
 
 /**
  * Application setup for Flurry, JodaTime and Dagger
@@ -20,8 +23,13 @@ public class EntourageApplication extends Application {
 
     private EntourageComponent component;
 
+    private ArrayList<EntourageActivity> activities;
+
     @Override
     public void onCreate() {
+
+        activities = new ArrayList<>();
+
         super.onCreate();
 
         setupFlurry();
@@ -62,5 +70,30 @@ public class EntourageApplication extends Application {
         AuthenticationController authenticationController = application.component.getAuthenticationController();
         if (authenticationController == null) return null;
         return authenticationController.getUser();
+    }
+
+    public void onActivityCreated(EntourageActivity activity) {
+        activities.add(activity);
+    }
+
+    public void onActivityDestroyed(EntourageActivity activity) {
+        activities.remove(activity);
+    }
+
+    public LoginActivity getLoginActivity() {
+        for (EntourageActivity activity:activities) {
+            if (activity instanceof LoginActivity) {
+                return (LoginActivity)activity;
+            }
+        }
+        return null;
+    }
+
+    public void finishLoginActivity() {
+        LoginActivity loginActivity = getLoginActivity();
+        if (loginActivity != null) {
+            Log.d(null, "Finishing login activity");
+            loginActivity.finish();
+        }
     }
 }
