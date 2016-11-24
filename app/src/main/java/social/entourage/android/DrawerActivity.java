9dcf9d585a62d6974f767289e6433129c30226cf
Mihai Ionescu <mihai.ionescu@pontussoftware.ro>
2016-11-24 11:23:30 +0200

    Handling of invalid bundle when checking the main application intent

--- app/src/main/java/social/entourage/android/DrawerActivity.java
+++ app/src/main/java/social/entourage/android/DrawerActivity.java
@@ -564,9 +564,15 @@ public class DrawerActivity extends EntourageSecuredActivity
     @Subscribe
     public void checkIntentAction(OnCheckIntentActionEvent event) {
         switchToMapFragment();
+        Intent intent = getIntent();
+        if (intent == null || intent.getExtras() == null) {
+            intentAction = null;
+            intentTour = null;
+            return;
+        }
         mapEntourageFragment.checkAction(intentAction, intentTour);
         if (PushNotificationContent.TYPE_NEW_CHAT_MESSAGE.equals(intentAction) || PushNotificationContent.TYPE_JOIN_REQUEST_ACCEPTED.equals(intentAction)) {
-            Message message = (Message) getIntent().getExtras().getSerializable(PushNotificationService.PUSH_MESSAGE);
+            Message message = (Message) intent.getExtras().getSerializable(PushNotificationService.PUSH_MESSAGE);
             if (message != null) {
                 PushNotificationContent content = message.getContent();
                 if (content != null) {
@@ -579,7 +585,7 @@ public class DrawerActivity extends EntourageSecuredActivity
             }
         }
         else if (PushNotificationContent.TYPE_ENTOURAGE_INVITATION.equals(intentAction)) {
-            Message message = (Message) getIntent().getExtras().getSerializable(PushNotificationService.PUSH_MESSAGE);
+            Message message = (Message) intent.getExtras().getSerializable(PushNotificationService.PUSH_MESSAGE);
             if (message != null) {
                 PushNotificationContent content = message.getContent();
                 if (content != null) {
@@ -591,7 +597,7 @@ public class DrawerActivity extends EntourageSecuredActivity
             }
         }
         else if (PushNotificationContent.TYPE_INVITATION_STATUS.equals(intentAction)) {
-            Message message = (Message) getIntent().getExtras().getSerializable(PushNotificationService.PUSH_MESSAGE);
+            Message message = (Message) intent.getExtras().getSerializable(PushNotificationService.PUSH_MESSAGE);
             if (message != null) {
                 PushNotificationContent content = message.getContent();
                 if (content != null) {
