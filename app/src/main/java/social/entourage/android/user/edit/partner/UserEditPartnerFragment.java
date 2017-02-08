de5dca25c3a86972336785c819bfe8fe039fac16
Mihai Ionescu <mihai.ionescu@smarttech2000.com>
2017-02-08 19:06:15 +0200

    Screen 09.2.1 Fixed the display on android 4

--- app/src/main/java/social/entourage/android/user/edit/partner/UserEditPartnerFragment.java
+++ app/src/main/java/social/entourage/android/user/edit/partner/UserEditPartnerFragment.java
@@ -76,6 +76,7 @@ public class UserEditPartnerFragment extends EntourageDialogFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_user_edit_partner, container, false);
         ButterKnife.bind(this, v);
