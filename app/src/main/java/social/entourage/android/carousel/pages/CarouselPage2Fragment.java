be9d254e78eb21ec54325bc9076ce0ef117b765c
Mihai Ionescu <mihai.ionescu@smarttech2000.com>
2017-03-01 13:37:18 +0200

    EMA-1110 Fixed carousel p2 on 4.x devices

--- app/src/main/java/social/entourage/android/carousel/pages/CarouselPage2Fragment.java
+++ app/src/main/java/social/entourage/android/carousel/pages/CarouselPage2Fragment.java
@@ -3,9 +3,11 @@ package social.entourage.android.carousel.pages;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import social.entourage.android.R;
 
@@ -24,7 +26,14 @@ public class CarouselPage2Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_carousel_page2, container, false);
+        View v = inflater.inflate(R.layout.fragment_carousel_page2, container, false);
+
+        TextView textView = (TextView)v.findViewById(R.id.carousel_p2_content);
+        if (textView != null) {
+            textView.setText( Html.fromHtml(getString(R.string.carousel_p2_content)) );
+        }
+
+        return v;
     }
 
 }
