4bb7ee37f36b0e0964168e5405f1959e461e792c
Mihai Ionescu <mihai.ionescu@smarttech2000.com>
2017-03-28 14:34:12 +0300

    EMA-1129 Screen20 Added the capability to send an email related to the point of interest

--- app/src/main/java/social/entourage/android/guide/poi/ReadPoiActivity.java
+++ app/src/main/java/social/entourage/android/guide/poi/ReadPoiActivity.java
@@ -1,5 +1,7 @@
 package social.entourage.android.guide.poi;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -8,6 +10,7 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.flurry.android.FlurryAgent;
 
@@ -138,4 +141,30 @@ public class ReadPoiActivity extends EntourageActivity {
     protected void onCloseButtonClicked() {
         finish();
     }
+
+    @OnClick(R.id.poi_report_button)
+    protected void onReportButtonClicked() {
+        if (poi == null) {
+            return;
+        }
+        // Build the email intent
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));
+        // Set the email to
+        String[] addresses = {Constants.EMAIL_CONTACT};
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        // Set the subject
+        String title = poi.getName();
+        if (title == null) title = "";
+        String emailSubject = getString(R.string.poi_report_email_subject_format, title);
+        intent.putExtra(Intent.EXTRA_SUBJECT, emailSubject);
+        if (intent.resolveActivity(this.getPackageManager()) != null) {
+            // Start the intent
+            startActivity(intent);
+        } else {
+            // No Email clients
+            Toast.makeText(this, R.string.error_no_email, Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
