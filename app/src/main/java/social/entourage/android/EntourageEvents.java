8f1577f9d61e8aeb6ea62a6a804205fd0f1a6652
François Pellissier <francois.pellissier@gmail.com>
2017-10-18 10:14:55 +0200

    chore(flurry) update lib to 7.2.3 + other libs

--- app/src/main/java/social/entourage/android/EntourageEvents.java
+++ app/src/main/java/social/entourage/android/EntourageEvents.java
@@ -1,16 +1,31 @@
 package social.entourage.android;
 
+import android.util.Log;
+
 import com.flurry.android.FlurryAgent;
+import com.flurry.android.FlurryAgentListener;
 
 /**
  * Wrapper for sending events to different aggregators
  * Created by Mihai Ionescu on 03/10/2017.
  */
 
-public class EntourageEvents {
+public class EntourageEvents implements FlurryAgentListener {
+
+    private static String TAG = EntourageEvents.class.getSimpleName();
+    static boolean isFlurrySessionStarted = false;
+
+    @Override
+    public void onSessionStarted() {
+        isFlurrySessionStarted=true;
+    }
 
     public static void logEvent(String event) {
-        FlurryAgent.logEvent(event);
+        if(isFlurrySessionStarted) {
+            FlurryAgent.logEvent(event);
+        } else {
+            Log.w(TAG, "Trying to send to Flurry without an opened session: "+event);
+        }
         if(EntourageApplication.get().getMixpanel()!= null) {
             EntourageApplication.get().getMixpanel().track(event, null);
         }
