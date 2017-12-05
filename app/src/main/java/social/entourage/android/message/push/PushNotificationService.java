b51072a9076a6e2d9c23a851c6d13aca00c2506c
Mihai Ionescu <mihai.ionescu@smarttech2000.com>
2017-12-05 16:00:31 +0200

    EMA-1689 Fixed crashes in GcmBroadcastReceiver, upgrade it for android >= 7

--- app/src/main/java/social/entourage/android/message/push/PushNotificationService.java
+++ app/src/main/java/social/entourage/android/message/push/PushNotificationService.java
@@ -1,26 +1,32 @@
 package social.entourage.android.message.push;
 
 import android.app.IntentService;
+import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v4.app.JobIntentService;
 
 import social.entourage.android.api.model.Message;
 import social.entourage.android.api.tape.Events;
 import social.entourage.android.tools.BusProvider;
 
-public class PushNotificationService extends IntentService {
+public class PushNotificationService extends JobIntentService {
 
-    public PushNotificationService() {
-        super("PushNotificationService");
+    /**
+     * Unique job ID for this service.
+     */
+    static final int JOB_ID = 1000;
+
+    static void enqueueWork(Context context, Intent work) {
+        enqueueWork(context, PushNotificationService.class, JOB_ID, work);
     }
 
     @Override
-    protected void onHandleIntent(Intent intent) {
+    protected void onHandleWork(@NonNull final Intent intent) {
         Message message = PushNotificationManager.getMessageFromIntent(intent, getApplicationContext());
         PushNotificationManager.getInstance().handlePushNotification(message, this);
 
         BusProvider.getInstance().post(new Events.OnPushNotificationReceived(message));
-
-        GcmBroadcastReceiver.completeWakefulIntent(intent);
     }
 
 }
