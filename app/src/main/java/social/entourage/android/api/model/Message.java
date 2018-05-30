ReseauEntourage/entourage-android

commit 21dd0958ca4c4e8de5c7ec19719a759ced6b0fc2

chore(backup) don't backup GCM id anymore chore(check update) fix update dialog display chore(strings) add some plural strings

app/src/main/java/social/entourage/android/api/model/Message.java
@@ -121,14 +121,14 @@ public class Message implements Serializable {
             if (content != null) {
                 String contentType = content.getType();
                 if (PushNotificationContent.TYPE_NEW_CHAT_MESSAGE.equals(contentType)) {
-                    return context.getString(R.string.notification_text_chat_message, count);
+                    return context.getResources().getQuantityString(R.plurals.notification_text_chat_message, count);
                 }
                 if (PushNotificationContent.TYPE_NEW_JOIN_REQUEST.equals(contentType)) {
                     String notificationText = "";
                     if (content.isEntourageRelated()) {
-                        notificationText = context.getString(R.string.notification_text_join_request_entourage_multiple, count, content.getFeedItemName());
+                        notificationText = context.getResources().getQuantityString(R.plurals.notification_text_join_request_entourage_multiple, count, content.getFeedItemName());
                     } else {
-                        notificationText = context.getString(R.string.notification_text_join_request_tour_multiple, count);
+                        notificationText = context.getResources().getQuantityString(R.plurals.notification_text_join_request_tour_multiple, count);
                     }
                     return notificationText;
                 }

app/build.gradle
@@ -112,7 +112,7 @@ android {
 }
 
 dependencies {
-    def supportVersion = "27.0.2"
+    def supportVersion = "27.1.0"
     def playServicesVersion = "11.8.0"
     def daggerVersion = "2.14.1"
     def okHttpVersion = "3.9.1"

app/src/main/AndroidManifest.xml
@@ -45,7 +45,8 @@
         android:allowBackup="true"
         android:icon="@mipmap/ic_launcher"
         android:label="@string/app_name"
-        android:theme="@style/AppTheme">
+        android:theme="@style/AppTheme"
+        android:fullBackupContent="@xml/backup_descriptor">
 
         <meta-data
             android:name="com.google.android.geo.API_KEY"

app/src/main/java/social/entourage/android/Constants.java
@@ -13,9 +13,6 @@ public class Constants {
     public static final String SHARED_PREFERENCES_FILE = "entourage_shared_preferences";
     public static final String FILENAME_TAPE_QUEUE = "encounters_queue";
 
-    // Update Dialog
-    public static final String UPDATE_DIALOG_DISPLAYED = "update_dialog_displayed";
-
     // Request and result codes
     public static final int REQUEST_CREATE_ENCOUNTER = 1;
     public static final int RESULT_CREATE_ENCOUNTER_OK = 2;

app/src/main/java/social/entourage/android/DrawerPresenter.java
@@ -2,7 +2,6 @@ package social.entourage.android;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.location.Location;
 import android.net.Uri;
 import android.support.v4.app.FragmentManager;
@@ -56,19 +55,17 @@ public class DrawerPresenter {
     private final DrawerActivity activity;
     private final AppRequest appRequest;
     private final UserRequest userRequest;
-
-    private SharedPreferences defaultSharedPreferences;
+    private boolean checkForUpdate = true;
 
     // ----------------------------------
     // LIFECYCLE
     // ----------------------------------
 
     @Inject
-    public DrawerPresenter(final DrawerActivity activity, final AppRequest appRequest, final UserRequest userRequest) {
+    DrawerPresenter(final DrawerActivity activity, final AppRequest appRequest, final UserRequest userRequest) {
         this.activity = activity;
         this.appRequest = appRequest;
         this.userRequest = userRequest;
-        initializeSharedPreferences();
     }
 
     // ----------------------------------
@@ -124,44 +121,25 @@ public class DrawerPresenter {
     // PUBLIC METHODS
     // ----------------------------------
 
-    private void initializeSharedPreferences() {
-        defaultSharedPreferences = activity.getApplicationContext().getSharedPreferences(Constants.UPDATE_DIALOG_DISPLAYED, Context.MODE_PRIVATE);
-        defaultSharedPreferences.edit().putBoolean(Constants.UPDATE_DIALOG_DISPLAYED, false).commit();
-    }
-
     public void checkForUpdate() {
-        if (!defaultSharedPreferences.getBoolean(Constants.UPDATE_DIALOG_DISPLAYED, false)) {
+        if (checkForUpdate) {
             Call<ResponseBody> call = appRequest.checkForUpdate();
             call.enqueue(new Callback<ResponseBody>() {
                 @Override
                 public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-
-                }
-
-                @Override
-                public void onFailure(Call<ResponseBody> call, Throwable t) {
-                    /*if (error.getResponse().getStatus() == 426) {
-                        if (!BuildConfig.DEBUG) {
-                            displayAppUpdateDialog();
-                        }
-                    }*/
-                }
-            });
-            /*appRequest.checkForUpdate(new ResponseCallback() {
-                @Override
-                public void success(Response response) {}
-
-                @Override
-                public void failure(RetrofitError error) {
-                    Response response = error.getResponse();
-                    if (response != null && response.getStatus() == 426) {
+                    if (response.code() == 426) {
                         if (!BuildConfig.DEBUG) {
                             displayAppUpdateDialog();
                         }
                     }
                 }
-            });*/
-            defaultSharedPreferences.edit().putBoolean(Constants.UPDATE_DIALOG_DISPLAYED, true).commit();
+
+                @Override
+                public void onFailure(Call<ResponseBody> call, Throwable t) {
+                    Log.e("CheckForUpdate", "Error connecting to API");
+                }
+            });
+            checkForUpdate=false;
         }
     }
 

app/src/main/res/values/strings_notification.xml
@@ -1,10 +1,20 @@
 <?xml version="1.0" encoding="utf-8"?>
 <resources>
     <string name="notification_title_chat_message">Nouveaux messages</string>
-    <string name="notification_text_chat_message">Vous avez %1$d nouveaux messages</string>
+    <plurals name="notification_text_chat_message">
+        <item quantity="zero">Vous n'avez pas de nouveau message</item>
+        <item quantity="one">Vous avez %1$d nouveau message</item>
+        <item quantity="other">Vous avez %1$d nouveaux messages</item>
+    </plurals>
 
     <string name="notification_text_join_request_tour_single">@string/tour_join_request_received_message</string>
     <string name="notification_text_join_request_entourage_single">@string/entourage_join_request_received</string>
-    <string name="notification_text_join_request_tour_multiple">%1$d personnes souhaitent rejoindre votre maraude.</string>
-    <string name="notification_text_join_request_entourage_multiple">%1$d personnes souhaitent rejoindre votre action: %2$s</string>
+    <plurals name="notification_text_join_request_tour_multiple">
+        <item quantity="one">%1$d personne souhaite rejoindre votre maraude.</item>
+        <item quantity="other">%1$d personnes souhaitent rejoindre votre maraude.</item>
+    </plurals>
+    <plurals name="notification_text_join_request_entourage_multiple">
+        <item quantity="one">%1$d personne souhaite rejoindre votre action: %2$s</item>
+        <item quantity="other">%1$d personnes souhaitent rejoindre votre action: %2$s</item>
+    </plurals>
 </resources>

app/src/main/res/xml/backup_descriptor.xml
@@ -1,0 +1,8 @@
+<?xml version="1.0" encoding="utf-8"?>
+<full-backup-content>
+    <!-- TODO Remove the following "exclude" elements to make them a part of the auto backup -->
+    <!-- Exclude the shared preferences file that contains the GCM registrationId -->
+    <exclude
+        domain="sharedpref"
+        path="ENTOURAGE_GCM_DATA.xml" />
+</full-backup-content>

commit 08bb879877159ad8d6299b355e1534f419994205

fix(notifications): fix some notifications being incorrectly formatted
The bug was introduced in 21dd095:
https://github.com/ReseauEntourage/entourage-android/commit/21dd095#diff-63be1e4cadcf1b27c1f3e8957e0b94c7

The second argument to getQuantityString (quantity) is not used for
substitution, so we have to pass it twice.
https://developer.android.com/guide/topics/resources/string-resource#Plurals
https://developer.android.com/reference/android/content/res/Resources#getquantitystring_7

app/src/main/java/social/entourage/android/api/model/Message.java
@@ -121,14 +121,14 @@ public class Message implements Serializable {
             if (content != null) {
                 String contentType = content.getType();
                 if (PushNotificationContent.TYPE_NEW_CHAT_MESSAGE.equals(contentType)) {
-                    return context.getResources().getQuantityString(R.plurals.notification_text_chat_message, count);
+                    return context.getResources().getQuantityString(R.plurals.notification_text_chat_message, count, count);
                 }
                 if (PushNotificationContent.TYPE_NEW_JOIN_REQUEST.equals(contentType)) {
                     String notificationText = "";
                     if (content.isEntourageRelated()) {
-                        notificationText = context.getResources().getQuantityString(R.plurals.notification_text_join_request_entourage_multiple, count, content.getFeedItemName());
+                        notificationText = context.getResources().getQuantityString(R.plurals.notification_text_join_request_entourage_multiple, count , count, content.getFeedItemName());
                     } else {
-                        notificationText = context.getResources().getQuantityString(R.plurals.notification_text_join_request_tour_multiple, count);
+                        notificationText = context.getResources().getQuantityString(R.plurals.notification_text_join_request_tour_multiple, count, count);
                     }
                     return notificationText;
                 }