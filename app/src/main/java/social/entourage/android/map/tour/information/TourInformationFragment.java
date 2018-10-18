9021394731bd4b4238b0dcb9daf368ccdaa50ff9
Mihai Ionescu <mihai.ionescu@smarttech2000.com>
2018-10-18 18:15:11 +0300

    EMA-2451 Screen14.1 When closing an entourage, the new close feed item card is displayed

--- app/src/main/java/social/entourage/android/map/tour/information/TourInformationFragment.java
+++ app/src/main/java/social/entourage/android/map/tour/information/TourInformationFragment.java
@@ -1688,7 +1688,7 @@ public class TourInformationFragment extends EntourageDialogFragment implements 
         }
         TourPoint endPoint = feedItem.getEndPoint();
         if (feedItem.getType() == TimestampedObject.TOUR_CARD) {
-            Tour tour = (Tour)feedItem;
+            Tour tour = (Tour) feedItem;
             List<TourPoint> tourPointsList = tour.getTourPoints();
             if (tourPointsList.size() > 1) {
                 TourPoint startPoint = tourPointsList.get(0);
@@ -1699,17 +1699,21 @@ public class TourInformationFragment extends EntourageDialogFragment implements 
                     startPoint = p;
                 }
             }
+
+            TourTimestamp tourTimestamp = new TourTimestamp(
+                    feedItem.getEndTime(),
+                    feedItem.getEndTime() != null ? feedItem.getEndTime() : now,
+                    feedItem.getType(),
+                    FeedItem.STATUS_CLOSED,
+                    endPoint,
+                    duration,
+                    distance
+            );
+            discussionAdapter.addCardInfoAfterTimestamp(tourTimestamp);
+        } else if (feedItem.getType() == TimestampedObject.ENTOURAGE_CARD) {
+            // Retrieve the latest chat messages, which should contain the close feed message
+            if (presenter != null) presenter.getFeedItemMessages(oldestChatMessageDate);
         }
-        TourTimestamp tourTimestamp = new TourTimestamp(
-                feedItem.getEndTime(),
-                feedItem.getEndTime() != null ? feedItem.getEndTime() : now,
-                feedItem.getType(),
-                FeedItem.STATUS_CLOSED,
-                endPoint,
-                duration,
-                distance
-        );
-        discussionAdapter.addCardInfoAfterTimestamp(tourTimestamp);
     }
 
     private void scrollToLastCard() {
