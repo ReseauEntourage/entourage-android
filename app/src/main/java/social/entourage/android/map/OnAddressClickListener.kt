ba07621b2315e27b44d8134101d97e7838a13ef2
Julien ROBERT <julien.robert092@gmail.com>
2019-02-07 23:23:23 +0100

    EN-737 Extract address click listener and underline addresses

---
+++ app/src/main/java/social/entourage/android/map/OnAddressClickListener.kt
@@ -0,0 +1,24 @@
+package social.entourage.android.map
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.view.View
+
+import java.util.Locale
+
+class OnAddressClickListener(private val context: Context, private val address: String) : View.OnClickListener {
+
+    override fun onClick(v: View) {
+        val uri = Uri.parse(String.format(Locale.FRENCH, "geo:0,0?q=%s", address))
+        openExternalMap(uri)
+    }
+
+    private fun openExternalMap(geoLocation: Uri) {
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = geoLocation
+        if (intent.resolveActivity(context.packageManager) != null) {
+            context.startActivity(intent)
+        }
+    }
+}
