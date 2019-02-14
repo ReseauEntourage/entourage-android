b5a6312ea9f4796809214eb56a342936f3a76925
Francois Pellissier <francois.pellissier@gmail.com>
2019-02-14 14:09:18 +0100

    chore(lib) switch to AndroidX for PFP

--- app/src/pfp/java/social/entourage/android/authentification/login/slideshow/pages/LoginSlideshowPage1Fragment.java
+++ app/src/pfp/java/social/entourage/android/authentification/login/slideshow/pages/LoginSlideshowPage1Fragment.java
@@ -3,11 +3,11 @@ package social.entourage.android.authentification.login.slideshow.pages;
 
 import android.os.Bundle;
 import androidx.annotation.NonNull;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
 import social.entourage.android.R;
 
 /**
