4dc494a3deca209d5cd3745742ceb1a94753b71b
Francois Pellissier <francois.pellissier@gmail.com>
2019-04-11 10:05:43 +0200

    feature(bootombar) add a plus button (WIP)

--- app/src/entourage/java/social/entourage/android/navigation/BottomNavigationDataSource.java
+++ app/src/entourage/java/social/entourage/android/navigation/BottomNavigationDataSource.java
@@ -15,6 +15,7 @@ public class BottomNavigationDataSource extends BaseBottomNavigationDataSource {
         add(new NavigationItem(R.string.action_map, R.drawable.ic_navigation_map), new MapEntourageWithTourFragment(), MapEntourageWithTourFragment.TAG);
         //TODO add proper tag
         add(new NavigationItem(R.string.action_guide, R.drawable.ic_navigation_guide), new GuideMapEntourageFragment(), GuideMapEntourageFragment.TAG);
+        add(new NavigationItem(R.string.action_plus, R.drawable.ic_bar_plus), new MapEntourageFragment(), MapEntourageFragment.TAG);
         add(new NavigationItem(R.string.action_my_messages, R.drawable.ic_navigation_my_messages), new MyEntouragesFragment(), MyEntouragesFragment.TAG);
         add(new NavigationItem(R.string.action_menu, R.drawable.ic_navigation_menu), new SideMenuFragment(), SideMenuFragment.TAG);
 
