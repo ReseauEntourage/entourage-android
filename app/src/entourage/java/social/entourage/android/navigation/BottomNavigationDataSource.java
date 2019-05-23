e4924efe28f324814413b072243968111f50dc35
Francois Pellissier <francois.pellissier@gmail.com>
2019-05-23 14:25:22 +0200

    feature(bootombar) add a plus button (WIP)

--- app/src/entourage/java/social/entourage/android/navigation/BottomNavigationDataSource.java
+++ app/src/entourage/java/social/entourage/android/navigation/BottomNavigationDataSource.java
@@ -14,13 +14,13 @@ public class BottomNavigationDataSource extends BaseBottomNavigationDataSource {
     public BottomNavigationDataSource() {
         add(new NavigationItem(R.string.action_empty, R.drawable.ic_navigation_map), new MapEntourageWithTourFragment(), MapEntourageWithTourFragment.TAG);
         //TODO add proper tag
-        add(new NavigationItem(R.string.action_empty, R.drawable.ic_navigation_guide), new GuideMapEntourageFragment(), GuideMapEntourageFragment.TAG);
-        //add(new NavigationItem(R.string.action_empty, R.drawable.ic_bar_plus), new MapEntourageFragment(), MapEntourageFragment.TAG);
-        add(new NavigationItem(R.string.action_empty, R.drawable.ic_navigation_my_messages), new MyEntouragesFragment(), MyEntouragesFragment.TAG);
-        add(new NavigationItem(R.string.action_empty, R.drawable.ic_navigation_menu), new SideMenuFragment(), SideMenuFragment.TAG);
+        add(new NavigationItem(R.string.action_guide, R.drawable.ic_navigation_guide), new GuideMapEntourageFragment(), GuideMapEntourageFragment.TAG);
+        add(new NavigationItem(R.string.empty_description, R.drawable.ic_navigation_plus), new SideMenuFragment(), SideMenuFragment.TAG);
+        add(new NavigationItem(R.string.action_my_messages, R.drawable.ic_navigation_my_messages), new MyEntouragesFragment(), MyEntouragesFragment.TAG);
+        add(new NavigationItem(R.string.action_profile, R.drawable.ic_navigation_profile), new SideMenuFragment(), SideMenuFragment.TAG);
 
         guideTabIndex = 1;
-        myMessagesTabIndex=2;
+        myMessagesTabIndex=3;
     }
 
 }
