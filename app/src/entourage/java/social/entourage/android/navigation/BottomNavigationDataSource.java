36d3b699e0bb98aad70f877358597f1828d49e2c
Francois Pellissier <francois.pellissier@gmail.com>
2019-05-23 14:22:27 +0200

    feature(bootombar) add a plus button (WIP)

--- app/src/entourage/java/social/entourage/android/navigation/BottomNavigationDataSource.java
+++ app/src/entourage/java/social/entourage/android/navigation/BottomNavigationDataSource.java
@@ -12,12 +12,12 @@ import social.entourage.android.sidemenu.SideMenuFragment;
 public class BottomNavigationDataSource extends BaseBottomNavigationDataSource {
 
     public BottomNavigationDataSource() {
-        add(new NavigationItem(R.string.action_map, R.drawable.ic_navigation_map), new MapEntourageWithTourFragment(), MapEntourageWithTourFragment.TAG);
+        add(new NavigationItem(R.string.action_empty, R.drawable.ic_navigation_map), new MapEntourageWithTourFragment(), MapEntourageWithTourFragment.TAG);
         //TODO add proper tag
-        add(new NavigationItem(R.string.action_guide, R.drawable.ic_navigation_guide), new GuideMapEntourageFragment(), GuideMapEntourageFragment.TAG);
-        add(new NavigationItem(R.string.action_plus, R.drawable.ic_bar_plus), new MapEntourageFragment(), MapEntourageFragment.TAG);
-        add(new NavigationItem(R.string.action_my_messages, R.drawable.ic_navigation_my_messages), new MyEntouragesFragment(), MyEntouragesFragment.TAG);
-        add(new NavigationItem(R.string.action_menu, R.drawable.ic_navigation_menu), new SideMenuFragment(), SideMenuFragment.TAG);
+        add(new NavigationItem(R.string.action_empty, R.drawable.ic_navigation_guide), new GuideMapEntourageFragment(), GuideMapEntourageFragment.TAG);
+        //add(new NavigationItem(R.string.action_empty, R.drawable.ic_bar_plus), new MapEntourageFragment(), MapEntourageFragment.TAG);
+        add(new NavigationItem(R.string.action_empty, R.drawable.ic_navigation_my_messages), new MyEntouragesFragment(), MyEntouragesFragment.TAG);
+        add(new NavigationItem(R.string.action_empty, R.drawable.ic_navigation_menu), new SideMenuFragment(), SideMenuFragment.TAG);
 
         guideTabIndex = 1;
         myMessagesTabIndex=2;
