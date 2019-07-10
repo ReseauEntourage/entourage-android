19c216c89091c2b17a5a1100673960c5dcd6add7
Francois Pellissier <francois.pellissier@gmail.com>
2019-07-10 09:05:43 +0200

    feature(logo) new preprod logo EN-1283

--- app/src/main/java/social/entourage/android/api/model/User.java
+++ app/src/main/java/social/entourage/android/api/model/User.java
@@ -316,7 +316,7 @@ public class User implements Serializable {
     }
 
     public boolean isPro() {
-        return EntourageApplication.isEntourageApp() && TYPE_PRO.equals(type);
+        return false;//EntourageApplication.isEntourageApp() && TYPE_PRO.equals(type);
     }
 
     public TourAuthor asTourAuthor() {
