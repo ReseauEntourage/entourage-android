b7784fdf485f630f26b7023176d0c4934f028cef
Francois Pellissier <francois.pellissier@gmail.com>
2019-07-23 20:27:13 +0200

    fix(crash) EN-1353 fix crash in newsfeed tab

--- app/src/main/java/social/entourage/android/map/MapEntourageFragment.java
+++ app/src/main/java/social/entourage/android/map/MapEntourageFragment.java
@@ -212,9 +212,6 @@ public class MapEntourageFragment extends BaseMapEntourageFragment implements To
     @BindView(R.id.fragment_map_entourage_mini_cards)
     EntourageMiniCardsView miniCardsView;
 
-    @BindView(R.id.fragment_map_filter_button)
-    View filterButton;
-
     private NewsfeedAdapter newsfeedAdapter;
     private Timer refreshToursTimer;
 
@@ -737,7 +734,12 @@ public class MapEntourageFragment extends BaseMapEntourageFragment implements To
 
         EntourageEvents.logEvent(selectedTab == MapTabItem.ALL_TAB ? EntourageEvents.EVENT_FEED_TAB_ALL : EntourageEvents.EVENT_FEED_TAB_EVENTS);
 
-        filterButton.setVisibility(selectedTab == MapTabItem.ALL_TAB ? View.VISIBLE : View.GONE);
+        if(getActivity()!= null) {
+            View filterButton = getActivity().findViewById(R.id.fragment_map_filter_button);
+            if(filterButton!= null) {
+                filterButton.setVisibility(selectedTab == MapTabItem.ALL_TAB ? View.VISIBLE : View.GONE);
+            }
+        }
 
         resetFeed();
 
