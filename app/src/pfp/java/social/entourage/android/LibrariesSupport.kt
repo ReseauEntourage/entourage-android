0b2dcfa570e9259a7edac972e3f790173644813b
Francois Pellissier <francois.pellissier@gmail.com>
2019-09-26 13:14:55 +0200

    chore: small cleaning on Crashlytics

--- app/src/pfp/java/social/entourage/android/LibrariesSupport.kt
+++ app/src/pfp/java/social/entourage/android/LibrariesSupport.kt
@@ -1,8 +1,6 @@
 package social.entourage.android
 
 import android.content.Context
-import social.entourage.android.tools.log.CrashlyticsLog
-import timber.log.Timber
 
 /**
  * Libraries support class specific to Voisinage
@@ -12,17 +10,5 @@ class LibrariesSupport : BaseLibrariesSupport(){
 
     override fun setupLibraries(context: Context) {
         super.setupLibraries(context)
-        setupTimberTree()
-    }
-
-    // ----------------------------------
-    // Libraries setup
-    // ----------------------------------
-    private fun setupTimberTree() {
-        if (BuildConfig.DEBUG) {
-            Timber.plant(Timber.DebugTree())
-        } else {
-            Timber.plant(CrashlyticsLog())
-        }
     }
 }
