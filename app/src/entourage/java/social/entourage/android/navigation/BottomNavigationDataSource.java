37a639a64ca65c758bc4d7088f89b9a6b0b9d119
Francois Pellissier <francois.pellissier@gmail.com>
2019-11-13 12:27:15 +0100

    feature: EN-837 adding + icon in menu bar (WIP)

--- app/src/entourage/java/social/entourage/android/navigation/BottomNavigationDataSource.java
+++ app/src/entourage/java/social/entourage/android/navigation/BottomNavigationDataSource.java
@@ -13,8 +13,8 @@ public class BottomNavigationDataSource extends BaseBottomNavigationDataSource {
 
     public BottomNavigationDataSource() {
         MapEntourageWithTourFragment homeFragment = new MapEntourageWithTourFragment();
-        add(new NavigationItem(R.string.action_empty, R.drawable.ic_pin_drop_white_24dp), homeFragment, MapEntourageWithTourFragment.TAG);
-        add(new NavigationItem(R.string.action_empty, R.drawable.ic_map_white_24dp), new GuideMapEntourageFragment(), GuideMapEntourageFragment.TAG);
+        add(new NavigationItem(R.string.action_empty, R.drawable.ic_home_white_24dp), homeFragment, MapEntourageWithTourFragment.TAG);
+        add(new NavigationItem(R.string.action_empty, R.drawable.ic_location_city_white_24dp), new GuideMapEntourageFragment(), GuideMapEntourageFragment.TAG);
         add(new NavigationItem(R.string.empty_description, R.drawable.ic_add_circle_white_24dp), homeFragment, MapEntourageWithTourFragment.TAG);
         add(new NavigationItem(R.string.action_empty, R.drawable.ic_chat_bubble_outline_white_24dp), new MyEntouragesFragment(), MyEntouragesFragment.TAG);
         add(new NavigationItem(R.string.action_empty, R.drawable.ic_account_circle_outline_white_24dp), new SideMenuFragment(), SideMenuFragment.TAG);
