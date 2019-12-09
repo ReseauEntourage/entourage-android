10b293accd84cf6d420fa8787b74c80c9eba1f6f
Francois Pellissier <francois.pellissier@gmail.com>
2019-12-09 10:00:53 +0100

    chore: small refacto on bottom bar

--- app/src/main/java/social/entourage/android/DrawerActivity.java
+++ app/src/main/java/social/entourage/android/DrawerActivity.java
@@ -375,7 +375,9 @@ public class DrawerActivity extends EntourageSecuredActivity
             for (int i = 0; i < tabCount; i++) {
                 BaseBottomNavigationDataSource.NavigationItem navigationItem = navigationDataSource.getNavigationItemAtIndex(i);
                 TabLayout.Tab tab = tabLayout.newTab();
-                tab.setCustomView(i == navigationDataSource.getMyMessagesTabIndex()?R.layout.toolbar_view_with_badge: R.layout.toolbar_view);
+                if(i == navigationDataSource.getMyMessagesTabIndex()) {
+                    tab.setCustomView(R.layout.toolbar_view_with_badge);
+                }
                 tab.setText(navigationItem.getText());
                 tab.setIcon(navigationItem.getIcon(getApplicationContext()));
                 tabLayout.addTab(tab);
