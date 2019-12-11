9dfff140a1bcac29d232a33b44dbe95d411ba86d
Francois Pellissier <francois.pellissier@gmail.com>
2019-12-11 12:37:11 +0100

    chore: update libraries

--- app/src/entourage/java/social/entourage/android/navigation/BottomNavigationDataSource.java
+++ app/src/entourage/java/social/entourage/android/navigation/BottomNavigationDataSource.java
@@ -15,7 +15,7 @@ public class BottomNavigationDataSource extends BaseBottomNavigationDataSource {
         MapEntourageWithTourFragment homeFragment = new MapEntourageWithTourFragment();
         add(new NavigationItem(R.string.action_empty, R.drawable.ic_home_white_24dp), homeFragment, MapEntourageWithTourFragment.TAG);
         add(new NavigationItem(R.string.action_empty, R.drawable.ic_location_city_white_24dp), new GuideMapEntourageFragment(), GuideMapEntourageFragment.TAG);
-        add(new NavigationItem(R.string.empty_description, R.drawable.ic_add_circle_white_24dp), homeFragment, MapEntourageWithTourFragment.TAG);
+        add(new NavigationItem(R.string.action_empty, R.drawable.ic_add_circle_white_24dp), homeFragment, MapEntourageWithTourFragment.TAG);
         add(new NavigationItem(R.string.action_empty, R.drawable.ic_chat_bubble_outline_white_24dp), new MyEntouragesFragment(), MyEntouragesFragment.TAG);
         add(new NavigationItem(R.string.action_empty, R.drawable.ic_account_circle_outline_white_24dp), new SideMenuFragment(), SideMenuFragment.TAG);
 
