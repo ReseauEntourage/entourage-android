944963c7bb52c9658282c682d3bf647178d32e7d
Jr <jerome@mj-devs.fr>
2020-11-30 17:42:22 +0100

    [EN-558] change wording -> when about user not empty

--- app/src/main/java/social/entourage/android/user/edit/UserEditFragment.kt
+++ app/src/main/java/social/entourage/android/user/edit/UserEditFragment.kt
@@ -162,6 +162,14 @@ open class UserEditFragment  : EntourageDialogFragment(), FragmentListener {
             user_email?.text = user.email
             user_phone?.text = user.phone
             user_about?.text = user.about
+
+            if (user.about.length > 0) {
+                user_about_edit_button?.text = getString(R.string.user_about_mod_button)
+            }
+            else {
+                user_about_edit_button?.text = getString(R.string.user_about_edit_button)
+            }
+
             val organizationList: MutableList<BaseOrganization> = ArrayList()
             user.partner?.let { organizationList.add(it) }
             user.organization?.let { organizationList.add(it) }
