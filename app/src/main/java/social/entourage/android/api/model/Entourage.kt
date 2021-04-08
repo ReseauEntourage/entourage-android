b9acde8e817cbf7d97403b57f0f79fc61957ffcb
Jr <jerome@mj-devs.fr>
2021-04-08 16:33:59 +0200

    Fix wording

--- app/src/main/java/social/entourage/android/api/model/Entourage.kt
+++ app/src/main/java/social/entourage/android/api/model/Entourage.kt
@@ -88,7 +88,7 @@ class EntourageDemand : EntourageAction, Serializable {
     constructor(category: String?,
                 title: String, description: String, location: LocationPoint) : super(GROUPTYPE_ACTION_DEMAND, category, title, description, location)
     override fun getFeedTypeLong(context: Context): String {
-        return context.getString(R.string.entourage_type_format, context.getString(R.string.entourage_type_demand))
+        return context.getString(R.string.entourage_type_format_bis, context.getString(R.string.entourage_type_demand))
     }
 }
 
