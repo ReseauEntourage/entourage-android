291acb578e2f4bf339c0f07fb858d8385e01e14e
François Pellissier <francois.pellissier@gmail.com>
2021-04-27 13:11:08 +0200

    chore: improve non fatal messages in Crashlytics

--- app/src/main/java/social/entourage/android/tools/log/CrashlyticsLog.kt
+++ app/src/main/java/social/entourage/android/tools/log/CrashlyticsLog.kt
@@ -17,24 +17,21 @@
             if(tag!=null) crashlytics.setCustomKey(CRASHLYTICS_KEY_TAG, tag)
             crashlytics.setCustomKey(CRASHLYTICS_KEY_MESSAGE, message)
 
-            if (t == null) {
-                val e = Exception(message)
+            crashlytics.recordException(t
+                    ?: Exception(message).apply {
                 val st:MutableList<StackTraceElement> = ArrayList()
-                e.stackTrace.forEach { element ->
+                this.stackTrace.forEach { element ->
                     //Removing Timber functions from stacktrace (useless in Firebase)
-                    if (!element.className.contains("CrashlyticsLog") && !element.className.contains("timber.log.Timber")) st.add(element)
+                    if (!element.className.contains(this@CrashlyticsLog.javaClass.name) && !element.className.contains(CRASHLYTICS_TIMBER_CLASS)) st.add(element)
                 }
-                e.stackTrace = st.toTypedArray()
-
-                crashlytics.recordException(e)
-            } else {
-                crashlytics.recordException(t)
-            }
+                this.stackTrace = st.toTypedArray()
+            })
         }
 
         companion object {
             private const val CRASHLYTICS_KEY_PRIORITY = "priority"
             private const val CRASHLYTICS_KEY_TAG = "tag"
             private const val CRASHLYTICS_KEY_MESSAGE = "message"
+            private const val CRASHLYTICS_TIMBER_CLASS = "timber.log.Timber"
         }
     }
