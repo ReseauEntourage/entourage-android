b7f88a80e2a728746fb7cfb2cc58eed65e2cac67
Julien <julien.richer@neoxia.com>
2021-04-30 17:42:20 +0200

    Clean login and pre-onboarding tests

--- app/src/androidTest/java/social/entourage/android/PreOnboardingTest.kt
+++ app/src/androidTest/java/social/entourage/android/PreOnboardingTest.kt
@@ -8,15 +8,18 @@ import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
-import androidx.test.filters.LargeTest
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
 import org.hamcrest.Description
 import org.hamcrest.Matcher
 import org.hamcrest.Matchers.allOf
 import org.hamcrest.TypeSafeMatcher
+import org.junit.After
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import social.entourage.android.onboarding.pre_onboarding.PreOnboardingStartActivity
 
 @LargeTest
 @RunWith(AndroidJUnit4::class)
@@ -24,52 +27,79 @@ class PreOnboardingTest {
 
     @Rule
     @JvmField
-    var mActivityTestRule = ActivityScenarioRule(MainActivity::class.java)
+    var activityRule = ActivityScenarioRule(PreOnboardingStartActivity::class.java)
+
+    @Before
+    fun setUp() {
+        checkNoUserIsLoggedIn()
+    }
+
+    private fun checkNoUserIsLoggedIn() {
+        try {
+            activityRule.scenario.onActivity { activity ->
+                EntourageApplication[activity].components.authenticationController.logOutUser()
+            }
+        } catch (e: RuntimeException) {
+            e.printStackTrace()
+        }
+    }
+
+    @After
+    fun tearDown() {
+        checkNoUserIsLoggedIn()
+    }
 
     @Test
-    fun preOnboardingTest() {
+    fun fullPreOnboardingTest() {
         val nextButton = onView(
-                allOf(withId(R.id.ui_button_next), withText("Suivant"),
+                allOf(withId(R.id.ui_button_next), withText(R.string.pre_onboard_button_next),
                         childAtPosition(
                                 childAtPosition(
                                         withId(android.R.id.content),
                                         0),
                                 9),
                         isDisplayed()))
-        nextButton.perform(click())
 
-        val textView2 = onView(
+        val titleTv = onView(
                 allOf(withId(R.id.ui_tv_title),
                         withParent(withParent(withId(android.R.id.content))),
                         isDisplayed()))
-        textView2.check(matches(withText("Entraidez-vous et créez du lien !")))
 
         nextButton.perform(click())
+        titleTv.check(matches(withText(R.string.pre_onboard_tutorial_title2)))
+        nextButton.perform(click())
+        titleTv.check(matches(withText(R.string.pre_onboard_tutorial_title3)))
+        nextButton.perform(click())
+        titleTv.check(matches(withText(R.string.pre_onboard_tutorial_title4)))
+        nextButton.perform(click())
 
-        val textView3 = onView(
-                allOf(withId(R.id.ui_tv_title),
-                        withParent(withParent(withId(android.R.id.content))),
+        checkSignupAndLoginButtonsExist()
+    }
+
+    @Test
+    fun skipPreOnboardingTest() {
+        val connectButton = onView(
+                allOf(withId(R.id.ui_button_connect), withText(R.string.pre_onboard_button_connect),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                2),
                         isDisplayed()))
-        textView3.check(matches(withText("Participez à des événements solidaires")))
+        connectButton.perform(click())
 
-        nextButton.perform(click())
+        checkSignupAndLoginButtonsExist()
+    }
 
-        val textView4 = onView(
-                allOf(withId(R.id.ui_tv_title),
-                        withParent(withParent(withId(android.R.id.content))),
-                        isDisplayed()))
-        textView4.check(matches(withText("Entourage\nvous guide")))
-
-        nextButton.perform(click())
-
+    private fun checkSignupAndLoginButtonsExist() {
         val button = onView(
-                allOf(withId(R.id.ui_button_signup), withText("JE M’INSCRIS"),
+                allOf(withId(R.id.ui_button_signup), withText(R.string.pre_onboard_choice_signup),
                         withParent(withParent(withId(android.R.id.content))),
                         isDisplayed()))
         button.check(matches(isDisplayed()))
 
         val button2 = onView(
-                allOf(withId(R.id.ui_button_login), withText("J’AI DÉJÀ UN COMPTE"),
+                allOf(withId(R.id.ui_button_login), withText(R.string.pre_onboard_choice_login),
                         withParent(withParent(withId(android.R.id.content))),
                         isDisplayed()))
         button2.check(matches(isDisplayed()))
