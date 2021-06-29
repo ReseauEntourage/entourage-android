bcb79924c0f193d89a58ee2e0b0fe9cd7c72b175
Julien <julien.richer@neoxia.com>
2021-06-29 10:47:05 +0200

    Update LoginTest with 2 no internet connection tests

--- app/src/main/java/social/entourage/android/entourage/my/MyEntouragesFragment.kt
+++ app/src/main/java/social/entourage/android/entourage/my/MyEntouragesFragment.kt
@@ -286,11 +286,11 @@ class MyEntouragesFragment  : BaseDialogFragment(), BaseViewHolderListener, Load
     }
 
     override fun onNetworkException() {
-        myentourages_layout?.let {make(it, R.string.network_error, Snackbar.LENGTH_LONG).show()}
+        myentourages_layout?.let { make(it, R.string.network_error, Snackbar.LENGTH_LONG).show() }
     }
 
     override fun onServerException(throwable: Throwable) {
-        myentourages_layout?.let {make(it, R.string.network_error, Snackbar.LENGTH_LONG).show() }
+        myentourages_layout?.let { make(it, R.string.network_error, Snackbar.LENGTH_LONG).show() }
     }
 
     override fun onTechnicalException(throwable: Throwable) {
