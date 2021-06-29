f9a8d33197cc30e6715e952fe951afd2202e18df
Julien <julien.richer@neoxia.com>
2021-06-29 10:47:05 +0200

    Update LoginTest with 2 no internet connection tests

--- app/src/androidTest/java/social/entourage/android/LoginTest.kt
+++ app/src/androidTest/java/social/entourage/android/LoginTest.kt
@@ -1,20 +1,26 @@
 package social.entourage.android
 
+import android.view.WindowManager
 import android.view.autofill.AutofillManager
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.IdlingResource
 import androidx.test.espresso.NoMatchingViewException
-import androidx.test.espresso.action.TypeTextAction
-import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.Root
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.matcher.IntentMatchers
-import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import androidx.test.platform.app.InstrumentationRegistry
 import com.jakewharton.espresso.OkHttp3IdlingResource
+import org.hamcrest.Description
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -65,12 +71,12 @@ class LoginTest {
         checkNoUserIsLoggedIn()
         checkFirstConnectionScreen()
 
-        onView(ViewMatchers.withId(R.id.ui_login_phone_et_phone)).perform(TypeTextAction(BuildConfig.TEST_ACCOUNT_LOGIN), ViewActions.closeSoftKeyboard())
+        onView(withId(R.id.ui_login_phone_et_phone)).perform(typeText(BuildConfig.TEST_ACCOUNT_LOGIN), closeSoftKeyboard())
         closeAutofill()
-        onView(ViewMatchers.withId(R.id.ui_login_et_code)).perform(ViewActions.typeText(BuildConfig.TEST_ACCOUNT_PWD), ViewActions.closeSoftKeyboard())
+        onView(withId(R.id.ui_login_et_code)).perform(typeText(BuildConfig.TEST_ACCOUNT_PWD), closeSoftKeyboard())
         closeAutofill()
-        onView(ViewMatchers.withId(R.id.ui_login_button_signup)).perform(ViewActions.click())
-        onView(ViewMatchers.withText(R.string.login_error_title)).check(ViewAssertions.doesNotExist())
+        onView(withId(R.id.ui_login_button_signup)).perform(click())
+        onView(withText(R.string.login_error_title)).check(doesNotExist())
 
         checkLoginSuccessful()
         Intents.release()
@@ -81,12 +87,12 @@ class LoginTest {
         Intents.init()
         checkFirstConnectionScreen()
 
-        onView(ViewMatchers.withId(R.id.ui_login_phone_et_phone)).perform(ViewActions.typeText(BuildConfig.TEST_ACCOUNT_LOGIN.replaceFirst("\\+33".toRegex(), "0")), ViewActions.closeSoftKeyboard())
+        onView(withId(R.id.ui_login_phone_et_phone)).perform(typeText(BuildConfig.TEST_ACCOUNT_LOGIN.replaceFirst("\\+33".toRegex(), "0")), closeSoftKeyboard())
         closeAutofill()
-        onView(ViewMatchers.withId(R.id.ui_login_et_code)).perform(ViewActions.typeText(BuildConfig.TEST_ACCOUNT_PWD), ViewActions.closeSoftKeyboard())
+        onView(withId(R.id.ui_login_et_code)).perform(typeText(BuildConfig.TEST_ACCOUNT_PWD), closeSoftKeyboard())
         closeAutofill()
-        onView(ViewMatchers.withId(R.id.ui_login_button_signup)).perform(ViewActions.click())
-        onView(ViewMatchers.withText(R.string.login_error_title)).check(ViewAssertions.doesNotExist())
+        onView(withId(R.id.ui_login_button_signup)).perform(click())
+        onView(withText(R.string.login_error_title)).check(doesNotExist())
 
         checkLoginSuccessful()
         Intents.release()
@@ -97,11 +103,11 @@ class LoginTest {
         checkNoUserIsLoggedIn()
         checkFirstConnectionScreen()
 
-        onView(ViewMatchers.withId(R.id.ui_login_phone_et_phone)).perform(ViewActions.typeText(BuildConfig.TEST_ACCOUNT_LOGIN), ViewActions.closeSoftKeyboard())
+        onView(withId(R.id.ui_login_phone_et_phone)).perform(typeText(BuildConfig.TEST_ACCOUNT_LOGIN), closeSoftKeyboard())
         closeAutofill()
-        onView(ViewMatchers.withId(R.id.ui_login_et_code)).perform(ViewActions.typeText("999999"), ViewActions.closeSoftKeyboard())
+        onView(withId(R.id.ui_login_et_code)).perform(typeText("999999"), closeSoftKeyboard())
         closeAutofill()
-        onView(ViewMatchers.withId(R.id.ui_login_button_signup)).perform(ViewActions.click())
+        onView(withId(R.id.ui_login_button_signup)).perform(click())
 
         checkLoginFailure()
     }
@@ -110,13 +116,52 @@ class LoginTest {
     fun loginFailureWrongPhoneNumberFormat() {
         checkFirstConnectionScreen()
 
-        onView(ViewMatchers.withId(R.id.ui_login_phone_et_phone)).perform(ViewActions.typeText("012345678"), ViewActions.closeSoftKeyboard())
+        onView(withId(R.id.ui_login_phone_et_phone)).perform(typeText("012345678"), closeSoftKeyboard())
         closeAutofill()
-        onView(ViewMatchers.withId(R.id.ui_login_et_code)).perform(ViewActions.typeText("000000"), ViewActions.closeSoftKeyboard())
+        onView(withId(R.id.ui_login_et_code)).perform(typeText("000000"), closeSoftKeyboard())
         closeAutofill()
-        onView(ViewMatchers.withId(R.id.ui_login_button_signup)).perform(ViewActions.click())
+        onView(withId(R.id.ui_login_button_signup)).perform(click())
 
         checkLoginFailure()
+    }
+
+    @Test
+    fun loginFailureNoInternetConnection() {
+        //Disable wifi and data
+        enableWifiAndData(false)
+
+        //Try to login
+        checkFirstConnectionScreen()
+
+        onView(withId(R.id.ui_login_phone_et_phone)).perform(typeText(BuildConfig.TEST_ACCOUNT_LOGIN), closeSoftKeyboard())
+        closeAutofill()
+        onView(withId(R.id.ui_login_et_code)).perform(typeText(BuildConfig.TEST_ACCOUNT_PWD), closeSoftKeyboard())
+        closeAutofill()
+        onView(withId(R.id.ui_login_button_signup)).perform(click())
+
+        //Check that error is displayed
+        onView(withText(R.string.login_error_network)).check(matches(isDisplayed()))
+        onView(withText(R.string.login_retry_label)).perform(click())
+
+        //Enable wifi and data
+        enableWifiAndData(true)
+    }
+
+    @Test
+    fun resendCodeFailureNoInternetConnection() {
+        //Disable wifi and data
+        enableWifiAndData(false)
+
+        //Try to resend code
+        onView(withId(R.id.ui_login_phone_et_phone)).perform(typeText(BuildConfig.TEST_ACCOUNT_LOGIN), closeSoftKeyboard())
+        closeAutofill()
+        onView(allOf(withId(R.id.ui_login_button_resend_code), isDisplayed())).perform(click())
+
+        //Check that error is displayed
+        onView(withText(R.string.login_error_network)).inRoot(SignUpTest.ToastMatcher()).check(matches(isDisplayed()))
+
+        //Enable wifi and data
+        enableWifiAndData(true)
     }
 
     private fun checkLoginSuccessful() {
@@ -124,8 +169,8 @@ class LoginTest {
     }
 
     private fun checkLoginFailure() {
-        onView(ViewMatchers.withText(R.string.login_error_title)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-        onView(ViewMatchers.withText(R.string.login_retry_label)).perform(ViewActions.click())
+        onView(withText(R.string.login_error_title)).check(matches(isDisplayed()))
+        onView(withText(R.string.login_retry_label)).perform(click())
     }
 
     private fun closeAutofill() {
@@ -140,12 +185,38 @@ class LoginTest {
 
     private fun checkFirstConnectionScreen() {
         try {
-            onView(ViewMatchers.withId(R.id.ui_button_login)).apply {
-                check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-                perform(ViewActions.click())
+            onView(withId(R.id.ui_button_login)).apply {
+                check(matches(isDisplayed()))
+                perform(click())
             }
         } catch (e: NoMatchingViewException) {
             Timber.w(e)
         }
+    }
+
+    private fun enableWifiAndData(enable: Boolean) {
+        val parameter = if (enable) "enable" else "disable"
+        InstrumentationRegistry.getInstrumentation().uiAutomation.apply {
+            executeShellCommand("svc wifi $parameter")
+            executeShellCommand("svc data $parameter")
+        }
+    }
+
+    class ToastMatcher : TypeSafeMatcher<Root>() {
+        override fun matchesSafely(item: Root?): Boolean {
+            item?.windowLayoutParams?.get()?.type?.let { type ->
+                if (type == WindowManager.LayoutParams.TYPE_TOAST) {
+                    val windowToken = item.decorView.windowToken
+                    val appToken = item.decorView.applicationWindowToken
+                    if (windowToken == appToken) {
+                        //Means this window isn't contained by any other windows
+                        return true
+                    }
+                }
+            }
+            return false
+        }
+
+        override fun describeTo(description: Description?) {}
     }
 }
