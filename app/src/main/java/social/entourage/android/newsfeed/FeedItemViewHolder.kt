ea00861c3280356c16cd483f455552ea36b79565
Jr <jerome@mj-devs.fr>
2021-10-15 16:11:15 +0200

    Fixs [EN-4240][EN-4244][EN-4253]

--- app/src/main/java/social/entourage/android/newsfeed/FeedItemViewHolder.kt
+++ app/src/main/java/social/entourage/android/newsfeed/FeedItemViewHolder.kt
@@ -194,15 +194,15 @@ open class FeedItemViewHolder(itemView: View) : BaseCardViewHolder(itemView) {
         }
 
         //last message
-        EntourageApplication.get().me()?.let { currentUser ->
-            itemView.tour_card_last_message?.text = feedItem.lastMessage?.getText(currentUser.id)
-                    ?: ""
-        } ?: kotlin.run { itemView.tour_card_last_message?.text = "" }
-
-        itemView.tour_card_last_message?.visibility = if (itemView.tour_card_last_message?.text.isNullOrBlank()) View.GONE else View.VISIBLE
-        itemView.tour_card_last_message?.setTypeface(null, if (feedItem.getUnreadMsgNb() == 0) Typeface.NORMAL else Typeface.BOLD)
-        itemView.tour_card_last_message?.setTextColor(if (feedItem.getUnreadMsgNb() == 0) ContextCompat.getColor(itemView.context, R.color.feeditem_card_details_normal) else ContextCompat.getColor(itemView.context, R.color.feeditem_card_details_bold))
-
+//        EntourageApplication.get().me()?.let { currentUser ->
+//            itemView.tour_card_last_message?.text = feedItem.lastMessage?.getText(currentUser.id)
+//                    ?: ""
+//        } ?: kotlin.run { itemView.tour_card_last_message?.text = "" }
+//
+//        itemView.tour_card_last_message?.visibility = if (itemView.tour_card_last_message?.text.isNullOrBlank()) View.GONE else View.VISIBLE
+//        itemView.tour_card_last_message?.setTypeface(null, if (feedItem.getUnreadMsgNb() == 0) Typeface.NORMAL else Typeface.BOLD)
+//        itemView.tour_card_last_message?.setTextColor(if (feedItem.getUnreadMsgNb() == 0) ContextCompat.getColor(itemView.context, R.color.feeditem_card_details_normal) else ContextCompat.getColor(itemView.context, R.color.feeditem_card_details_bold))
+        itemView.tour_card_last_message?.visibility = View.GONE
         //last update date
         itemView.tour_card_last_update_date?.text = formatLastUpdateDate(feedItem.updatedTime, itemView.context)
         itemView.tour_card_last_update_date?.setTypeface(null, if (feedItem.getUnreadMsgNb() == 0) Typeface.NORMAL else Typeface.BOLD)
