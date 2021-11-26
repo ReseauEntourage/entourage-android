95835aeca536c7f97505d63e743ed2c6a53cec08
Jr <jerome@mj-devs.fr>
2021-11-26 17:08:21 +0100

    [EN-4225] add cornerRadius -> Announce image

--- app/src/main/java/social/entourage/android/home/HomeViewsHolder.kt
+++ app/src/main/java/social/entourage/android/home/HomeViewsHolder.kt
@@ -10,6 +10,8 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
 import kotlinx.android.synthetic.main.layout_cell_action_more.view.*
@@ -66,7 +68,8 @@ class AnnounceVH(view: View) : RecyclerView.ViewHolder(view) {
                     Glide.with(imageView.context)
                             .load(Uri.parse(imageUrl))
                             .listener(requestListener)
-                        .placeholder(R.drawable.bg_button_rounded_pre_onboard_orange_plain)
+                            .transform(CenterCrop(),RoundedCorners(8))
+                            .placeholder(R.drawable.bg_button_rounded_pre_onboard_orange_plain)
                             .into(imageView)
                 }
             }
