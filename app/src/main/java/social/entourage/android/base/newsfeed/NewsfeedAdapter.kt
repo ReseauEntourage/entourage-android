1e9762e7b7bb41ea7335655d3f721744e18e5b82
Jr <jerome@mj-devs.fr>
2021-12-07 18:29:30 +0100

    [EN-4445] change cells -> newsfeed with map

--- app/src/main/java/social/entourage/android/base/newsfeed/NewsfeedAdapter.kt
+++ app/src/main/java/social/entourage/android/base/newsfeed/NewsfeedAdapter.kt
@@ -39,7 +39,7 @@ class NewsfeedAdapter : HeaderFooterBaseAdapter() {
         )
         viewHolderFactory.registerViewHolder(
                 TimestampedObject.ENTOURAGE_CARD,
-                ViewHolderType(EntourageViewHolder::class.java, FeedItemViewHolder.layoutResource)
+                ViewHolderType(EntourageViewHolder::class.java, FeedItemViewHolderRenew.layoutResource)
         )
         viewHolderFactory.registerViewHolder(
                 TimestampedObject.ANNOUNCEMENT_CARD,
