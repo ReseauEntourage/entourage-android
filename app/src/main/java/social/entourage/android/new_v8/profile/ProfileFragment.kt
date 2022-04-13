b86cf6fc8a9ab83a04d5467b44696390c6638dd3
Salim MERCHERMEK <salim@Salims-MacBook-Pro.local>
2022-04-13 11:22:20 +0200

    Add Test Page for user profile

--- app/src/main/java/social/entourage/android/new_v8/profile/ProfileFragment.kt
+++ app/src/main/java/social/entourage/android/new_v8/profile/ProfileFragment.kt
@@ -1,13 +1,10 @@
 package social.entourage.android.new_v8.profile
 
-import android.content.Context
-import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
@@ -17,14 +14,8 @@ import retrofit2.Callback
 import retrofit2.Response
 import social.entourage.android.EntourageApplication
 import social.entourage.android.R
-import social.entourage.android.api.MetaDataRepository
-import social.entourage.android.api.model.Tags
-import social.entourage.android.api.request.MetaDataResponse
 import social.entourage.android.api.request.UserResponse
 import social.entourage.android.databinding.NewFragmentProfileBinding
-import social.entourage.android.onboarding.pre_onboarding.PreOnboardingStartActivity
-import social.entourage.android.tools.log.AnalyticsEvents
-import timber.log.Timber
 
 
 class ProfileFragment : Fragment() {
@@ -50,7 +41,7 @@ class ProfileFragment : Fragment() {
         getUser()
 
         binding.imageProfile.setOnClickListener {
-            findNavController().navigate(R.id.action_profile_fragment_to_create_group_fragment)
+            findNavController().navigate(R.id.action_profile_fragment_to_test_fragment)
         }
     }
 
