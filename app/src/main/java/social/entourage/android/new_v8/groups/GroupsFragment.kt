1aeea0f2cbb2ea12eb8d63b1bcbfca7a6ecd376b
Salim MERCHERMEK <salim@Salims-MacBook-Pro.local>
2022-06-09 14:12:57 +0200

    Sprint review

--- app/src/main/java/social/entourage/android/new_v8/groups/GroupsFragment.kt
+++ app/src/main/java/social/entourage/android/new_v8/groups/GroupsFragment.kt
@@ -1,12 +1,10 @@
 package social.entourage.android.new_v8.groups
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import com.google.android.material.appbar.AppBarLayout
 import com.google.android.material.tabs.TabLayoutMediator
