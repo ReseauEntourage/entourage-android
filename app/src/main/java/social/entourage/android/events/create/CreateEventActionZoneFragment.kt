83794989f07ddcc6f8e0c91ecc80deffbf23be92
NotMe <>
2023-01-09 16:04:03 +0100

    [EN-5450] mod wording

--- app/src/main/java/social/entourage/android/events/create/CreateEventActionZoneFragment.kt
+++ app/src/main/java/social/entourage/android/events/create/CreateEventActionZoneFragment.kt
@@ -12,9 +12,9 @@ class CreateEventActionZoneFragment : UserActionPlaceFragment() {
     // To be changed
     override fun setupViews() {
         super.setupViews()
-        ui_onboard_place_tv_title?.text = getString(R.string.profile_edit_zone_title)
-        ui_onboard_place_tv_info?.text = getString(R.string.profile_edit_zone_description)
-
+        ui_onboard_place_tv_title?.text = getString(R.string.event_location_title)
+        ui_onboard_place_tv_info?.text = getString(R.string.event_location_detail)
+        ui_onboard_phone_tv_info2?.visibility = View.INVISIBLE
         edit_place_title_layout?.visibility = View.VISIBLE
         edit_place_title_layout?.title_action_button?.setOnClickListener {
             validate()
