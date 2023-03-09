647e03ccc6654e1f120585f2f66e3d82ed1019d7
Clément Perrousset <clement.perrousset@gmail.com>
2023-03-09 09:21:04 +0100

    chore(CommentlistAdapter) : build Nio

--- app/src/main/java/social/entourage/android/comment/CommentsListAdapter.kt
+++ app/src/main/java/social/entourage/android/comment/CommentsListAdapter.kt
@@ -200,7 +200,7 @@ class CommentsListAdapter(
             }
 
             if (isMe || isConversation) {
-                binding.report.visibility = View.GONE
+                binding.report.visibility = View.VISIBLE
             }
             else {
                 binding.report.visibility = View.VISIBLE
