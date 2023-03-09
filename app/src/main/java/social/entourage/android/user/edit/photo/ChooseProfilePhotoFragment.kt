98210b82781b0bdc806a48918a3f8bdc4e814a87
Clément Perrousset <clement.perrousset@gmail.com>
2023-03-09 09:20:48 +0100

    chore(commentListAdapter) : build Nico

--- app/src/main/java/social/entourage/android/user/edit/photo/ChooseProfilePhotoFragment.kt
+++ app/src/main/java/social/entourage/android/user/edit/photo/ChooseProfilePhotoFragment.kt
@@ -38,6 +38,7 @@ class ChooseProfilePhotoFragment : EditPhotoFragment() {
         if (context is PhotoChooseInterface) {
             mListener = requireContext() as PhotoChooseInterface
         }
+
         setBackButton()
         updateUserView()
     }
