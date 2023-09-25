01a607bdf748302c8fd35ccae75e22e4a360a39b
clément perrousset <clement.perrousset@gmail.com>
2023-09-25 11:48:13 +0200

    feature(HomeV2Fragment) improve animation

--- app/src/main/java/social/entourage/android/homev2/HomeV2Fragment.kt
+++ app/src/main/java/social/entourage/android/homev2/HomeV2Fragment.kt
@@ -1,5 +1,8 @@
 package social.entourage.android.homev2
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ValueAnimator
 import android.content.Intent
 import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
@@ -7,6 +10,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.animation.doOnEnd
 import androidx.core.content.res.ResourcesCompat
 import androidx.core.widget.NestedScrollView
 import androidx.fragment.app.Fragment
@@ -15,6 +19,7 @@ import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.google.gson.Gson
+import kotlinx.android.synthetic.main.new_fragment_edit_group.nestedScrollView
 import social.entourage.android.EntourageApplication
 import social.entourage.android.MainActivity
 import social.entourage.android.R
@@ -59,9 +64,10 @@ class HomeV2Fragment: Fragment() {
     private var currentSectionsFilters = ActionSectionFilters()
     private var user: User? = null
     private val NEW_MARGIN = 10
-    private val DEFAULT_MARGIN = 40
+    private val DEFAULT_MARGIN = 80
     private val NEW_MARGIN_LOGO = 10
     private val DEFAULT_MARGIN_LOGO = 30
+    private var isAnimating = false
 
 
 
@@ -105,7 +111,6 @@ class HomeV2Fragment: Fragment() {
         if(isAdded){
             val meId = EntourageApplication.get().me()?.id
             if(meId == null) return
-            homePresenter.getSummary()
             homePresenter.getMyGroups(pagegroup,nbOfItemForHozrizontalList,meId)
             homePresenter.getAllEvents(pageEvent,nbOfItemForHozrizontalList,currentFilters.travel_distance(),currentFilters.latitude(),currentFilters.longitude(),"future")
             homePresenter.getAllDemands(0,nbOfItemForVerticalList,currentFilters.travel_distance(),currentFilters.latitude(),currentFilters.longitude(),currentSectionsFilters.getSectionsForWS())
@@ -172,14 +177,14 @@ class HomeV2Fragment: Fragment() {
         if(allGroup == null){
             return
         }
-        this.homeGroupAdapter.resetData(allGroup!!)
+        this.homeGroupAdapter.resetData(allGroup)
     }
 
     fun handleEvent(allEvent: MutableList<Events>?){
         if(allEvent == null){
             return
         }
-        this.homeEventAdapter.resetData(allEvent!!)
+        this.homeEventAdapter.resetData(allEvent)
 
     }
     fun handleAction(allAction: MutableList<Action>?){
@@ -187,7 +192,8 @@ class HomeV2Fragment: Fragment() {
             //
             return
         }
-        this.homeActionAdapter.resetData(allAction!!)
+
+        this.homeActionAdapter.resetData(allAction)
 
     }
     fun handlePedago(allPedago: MutableList<Pedago>?){
@@ -200,6 +206,16 @@ class HomeV2Fragment: Fragment() {
             pedagos.add(allPedago[k])
         }
         this.homePedagoAdapter?.resetData(pedagos)
+        for(pedago in allPedago){
+            if(pedago.name!!.contains("Comment créer")){
+                Log.wtf("wtf", "pedago name " + pedago.name + " pedago id " + pedago.id)
+            }
+            if(pedago.name!!.contains("points communs")){
+                Log.wtf("wtf", "pedago name " + pedago.name + " pedago id " + pedago.id)
+            }
+        }
+        homePresenter.getSummary()
+
     }
 
     private fun updateContributionsView(summary: Summary) {
@@ -207,6 +223,8 @@ class HomeV2Fragment: Fragment() {
     }
 
     fun handleHelps(summary: Summary){
+
+
         val formattedString = requireContext().getString(R.string.home_v2_help_title_three, summary.moderator?.displayName)
 
         val help1 = Help(requireContext().getString(R.string.home_v2_help_title_one) , R.drawable.first_help_item_illu)
@@ -278,28 +296,49 @@ class HomeV2Fragment: Fragment() {
 
     }
 
-    private fun setNestedScrollViewAnimation(){
+    private fun setNestedScrollViewAnimation() {
         binding.homeNestedScrollView.setOnScrollChangeListener(NestedScrollView.OnScrollChangeListener { _, _, scrollY, _, _ ->
-            // récupérez la référence de votre élément en haut (par exemple, l'avatar)
             val layoutParamsProfile = binding.avatar.layoutParams as ViewGroup.MarginLayoutParams
             val layoutParamsNotif = binding.uiLayoutNotif.layoutParams as ViewGroup.MarginLayoutParams
             val layoutParamsLogo = binding.ivLogoHome.layoutParams as ViewGroup.MarginLayoutParams
-
-            // ajustez la marge en fonction de la position de défilement
-            if (scrollY > 50) {
-                layoutParamsProfile.topMargin = NEW_MARGIN // définissez la nouvelle marge
-                layoutParamsNotif.topMargin = NEW_MARGIN // définissez la nouvelle marge
-                layoutParamsLogo.topMargin = NEW_MARGIN_LOGO // définissez la nouvelle marge
-                binding.homeTitle.visibility = View.GONE
-            } else {
-                layoutParamsProfile.topMargin = DEFAULT_MARGIN // rétablissez la marge par défaut
-                layoutParamsNotif.topMargin = DEFAULT_MARGIN // rétablissez la marge par défaut
-                layoutParamsLogo.topMargin = DEFAULT_MARGIN_LOGO // rétablissez la marge par défaut
-                binding.homeTitle.visibility = View.VISIBLE
+            Log.wtf("wtf", "wtf scroll y " + scrollY)
+            if(scrollY == 0) isAnimating = false
+            if(isAnimating == false){
+                isAnimating = true
+                val animator = if (scrollY > 50) {
+                    ValueAnimator.ofInt(layoutParamsProfile.topMargin, NEW_MARGIN).apply {
+                        duration = 100
+                        addUpdateListener { animation ->
+                            val animatedValue = animation.animatedValue as Int
+                            layoutParamsProfile.topMargin = animatedValue
+                            layoutParamsNotif.topMargin = animatedValue
+                            layoutParamsLogo.topMargin = animatedValue
+                            binding.avatar.layoutParams = layoutParamsProfile
+                            binding.uiLayoutNotif.layoutParams = layoutParamsNotif
+                            binding.ivLogoHome.layoutParams = layoutParamsLogo
+                            binding.homeTitle.visibility = View.GONE
+                        }
+                    }
+                } else {
+                    ValueAnimator.ofInt(layoutParamsProfile.topMargin, DEFAULT_MARGIN).apply {
+                        duration = 100
+                        addUpdateListener { animation ->
+                            val animatedValue = animation.animatedValue as Int
+                            layoutParamsProfile.topMargin = animatedValue
+                            layoutParamsNotif.topMargin = animatedValue
+                            layoutParamsLogo.topMargin = animatedValue
+                            binding.avatar.layoutParams = layoutParamsProfile
+                            binding.uiLayoutNotif.layoutParams = layoutParamsNotif
+                            binding.ivLogoHome.layoutParams = layoutParamsLogo
+                            binding.homeTitle.visibility = View.VISIBLE
+                        }
+                    }
+                }
+                animator.doOnEnd {
+                    isAnimating = false
+                }
+                animator.start()
             }
-            binding.avatar.layoutParams = layoutParamsProfile
-            binding.uiLayoutNotif.layoutParams = layoutParamsNotif
-            binding.ivLogoHome.layoutParams = layoutParamsLogo
         })
     }
 }
