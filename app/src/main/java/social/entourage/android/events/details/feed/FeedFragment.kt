80a8e6b01e847410a4be4c0297a53a11dcceabb4
Clément Perrousset <clement.perrousset@gmail.com>
2024-02-02 16:09:58 +0100

    fix(new_layout_post) : fix UI reaction layout

--- app/src/main/java/social/entourage/android/events/details/feed/FeedFragment.kt
+++ app/src/main/java/social/entourage/android/events/details/feed/FeedFragment.kt
@@ -15,12 +15,14 @@ import androidx.core.content.ContextCompat.getColor
 import androidx.core.content.res.ResourcesCompat
 import androidx.core.os.bundleOf
 import androidx.core.view.isVisible
+import androidx.core.widget.NestedScrollView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.setFragmentResultListener
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
@@ -119,6 +121,7 @@ class FeedFragment : Fragment(), CallbackReportFragment, ReactionInterface {
         onFragmentResult()
         openLink()
         AnalyticsEvents.logEvent(AnalyticsEvents.Event_detail_main)
+        setupNestedScrollViewScrollListener()
     }
 
 
@@ -128,6 +131,32 @@ class FeedFragment : Fragment(), CallbackReportFragment, ReactionInterface {
             updateView()
         }
         handleImageViewAnimation()
+    }
+
+    private fun setupNestedScrollViewScrollListener() {
+        binding.scrollView.setOnScrollChangeListener(NestedScrollView.OnScrollChangeListener { _, _, scrollY, _, _ ->
+            if (scrollY > 0) {
+                Log.wtf("NestedScroll", "Scrolling in NestedScrollView")
+                // Ici, tu peux notifier ton RecyclerView adapter de cacher les éléments layoutReactions
+                hideReactionsInRecyclerView()
+            }
+        })
+    }
+
+    private fun hideReactionsInRecyclerView() {
+        hideReactionsInView(binding.postsNewRecyclerview)
+        hideReactionsInView(binding.postsOldRecyclerview)
+    }
+
+    private fun hideReactionsInView(recyclerView: RecyclerView) {
+        val firstVisiblePosition = (recyclerView.layoutManager as? LinearLayoutManager)?.findFirstVisibleItemPosition() ?: return
+        val lastVisiblePosition = (recyclerView.layoutManager as? LinearLayoutManager)?.findLastVisibleItemPosition() ?: return
+
+        // Parcours toutes les cellules visibles
+        for (i in firstVisiblePosition..lastVisiblePosition) {
+            val viewHolder = recyclerView.findViewHolderForAdapterPosition(i) as? PostAdapter.ViewHolder
+            viewHolder?.binding?.layoutReactions?.visibility = View.GONE
+        }
     }
 
     private fun handleImageViewAnimation() {
