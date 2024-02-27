f2b2f51f27129122152e2b794687310c3e6f4cbf
François Pellissier <francois.pellissier@gmail.com>
2024-02-27 12:00:30 +0100

    chore: removing synthetic from GDSMainActivity

--- app/src/main/java/social/entourage/android/guide/GDSMainActivity.kt
+++ app/src/main/java/social/entourage/android/guide/GDSMainActivity.kt
@@ -2,24 +2,26 @@ package social.entourage.android.guide
 
 import android.location.Location
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_g_d_s_main.*
 import social.entourage.android.R
 import social.entourage.android.base.BaseSecuredActivity
+import social.entourage.android.databinding.ActivityGDSMainBinding
 import timber.log.Timber
 
 class GDSMainActivity : BaseSecuredActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_g_d_s_main)
 
-        ui_bt_back?.setOnClickListener { onBackPressed() }
+        val binding:ActivityGDSMainBinding = ActivityGDSMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.uiBtBack.setOnClickListener { onBackPressed() }
         val guideFg = GuideMapFragment()
 
         val fragmentTransaction = supportFragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.ui_container, guideFg, GuideMapFragment.TAG)
         fragmentTransaction.commit()
 
-        ui_bt_search?.setOnClickListener {
+        binding.uiBtSearch.setOnClickListener {
             guideFg.map?.let {map ->
                 val region = map.projection.visibleRegion
                 val result = floatArrayOf(0f)
