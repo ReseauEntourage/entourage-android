0daeedf273428fe1a999745106523779370d5b3f
clément perrousset <clement.perrousset@gmail.com>
2024-06-18 09:58:13 +0200

    chore(VersionName) : 9.8 -> 9.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
 
     // App versions
     val versionMajor = 9
-    val versionMinor = 8
+    val versionMinor = 9
     val versionPatch = "git rev-list HEAD --count".runCommand().toInt()
     val versionBranchName = "git rev-parse --abbrev-ref HEAD".runCommand()
     val versionCodeInt = (versionMajor * 100 + versionMinor) * 10000 + versionPatch % 10000
