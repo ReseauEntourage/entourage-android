b2ec60b67b7722c6382076471466f23334620384
clément perrousset <clement.perrousset@gmail.com>
2024-07-18 14:54:19 +0200

    fix(ActionListFramgent) : correct pagination on action

--- app/src/main/java/social/entourage/android/actions/list/ActionListFragment.kt
+++ app/src/main/java/social/entourage/android/actions/list/ActionListFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import social.entourage.android.EntourageApplication
 import social.entourage.android.actions.ActionsPresenter
 import social.entourage.android.api.model.Action
@@ -61,7 +62,6 @@ class ActionListFragment : Fragment() {
         isContrib = actionsPresenter.isContrib
         reloadActions()
         binding.progressBar.visibility = View.VISIBLE
-
     }
 
     private fun initializeSwipeRefresh() {
@@ -69,40 +69,45 @@ class ActionListFragment : Fragment() {
             reloadActions()
         }
     }
+
     fun reloadActions() {
         page = 0
         demandeActions.clear()
         actionAdapter.notifyDataSetChanged()
+        actionsPresenter.isLastPage = false
         loadAction()
         Log.d("ActionListFragment", "reloadActions")
     }
 
-    fun loadAction(){
+    fun loadAction() {
+        if (actionsPresenter.isLoading || actionsPresenter.isLastPage) return
+        actionsPresenter.isLoading = true
+
         page++
-        if(isSearching){
-            if(isContrib){
+        if (isSearching) {
+            if (isContrib) {
                 Log.d("ActionListFragment", "isSearching contrib")
                 actionsPresenter.getAllContribsWithSearchQuery(query, page, EVENTS_PER_PAGE)
-            }else{
+            } else {
                 Log.d("ActionListFragment", "isSearching demand")
                 actionsPresenter.getAllDemandsWithSearchQuery(query, page, EVENTS_PER_PAGE)
             }
-        }else if(MainFilterActivity.hasFilter){
+        } else if (MainFilterActivity.hasFilter) {
             val sections = MainFilterActivity.savedActionInterests.joinToString(",")
-            if(isContrib){
+            if (isContrib) {
                 Log.d("ActionListFragment", "sections contrib")
-                actionsPresenter.getAllContribsWithFilter(page, EVENTS_PER_PAGE,MainFilterActivity.savedRadius, MainFilterActivity.savedLocation?.lat, MainFilterActivity.savedLocation?.lng, sections)
-            }else{
+                actionsPresenter.getAllContribsWithFilter(page, EVENTS_PER_PAGE, MainFilterActivity.savedRadius, MainFilterActivity.savedLocation?.lat, MainFilterActivity.savedLocation?.lng, sections)
+            } else {
                 Log.d("ActionListFragment", "sections demand")
-                actionsPresenter.getAllDemandsWithFilter(page, EVENTS_PER_PAGE,MainFilterActivity.savedRadius, MainFilterActivity.savedLocation?.lat, MainFilterActivity.savedLocation?.lng, sections)
+                actionsPresenter.getAllDemandsWithFilter(page, EVENTS_PER_PAGE, MainFilterActivity.savedRadius, MainFilterActivity.savedLocation?.lat, MainFilterActivity.savedLocation?.lng, sections)
             }
-        }else{
-            if(isContrib){
+        } else {
+            if (isContrib) {
                 Log.d("ActionListFragment", "all contrib")
-                actionsPresenter.getAllContribs(page, EVENTS_PER_PAGE,  currentFilters.travel_distance(), currentFilters.latitude(), currentFilters.longitude(), null)
-            }else{
+                actionsPresenter.getAllContribs(page, EVENTS_PER_PAGE, currentFilters.travel_distance(), currentFilters.latitude(), currentFilters.longitude(), null)
+            } else {
                 Log.d("ActionListFragment", "all demand")
-                actionsPresenter.getAllDemands(page, EVENTS_PER_PAGE,  currentFilters.travel_distance(), currentFilters.latitude(), currentFilters.longitude(), null)
+                actionsPresenter.getAllDemands(page, EVENTS_PER_PAGE, currentFilters.travel_distance(), currentFilters.latitude(), currentFilters.longitude(), null)
             }
         }
     }
@@ -115,6 +120,20 @@ class ActionListFragment : Fragment() {
         binding.recyclerView.apply {
             layoutManager = LinearLayoutManager(context)
             adapter = actionAdapter
+            addOnScrollListener(object : RecyclerView.OnScrollListener() {
+                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                    super.onScrolled(recyclerView, dx, dy)
+
+                    val layoutManager = recyclerView.layoutManager as LinearLayoutManager
+                    val totalItemCount = layoutManager.itemCount
+                    val visibleItemCount = layoutManager.childCount
+                    val firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition()
+
+                    if (!recyclerView.canScrollVertically(1) && !actionsPresenter.isLoading && !actionsPresenter.isLastPage) {
+                        loadAction()
+                    }
+                }
+            })
         }
     }
 
@@ -137,10 +156,11 @@ class ActionListFragment : Fragment() {
     }
 
     private fun handleActions(actions: List<Action>) {
-        if(actions.isEmpty()){
+        actionsPresenter.isLoading = false
+        if (actions.isEmpty() && page == 1) {
             binding.emptyStateLayout.visibility = View.VISIBLE
             binding.recyclerView.visibility = View.GONE
-        }else{
+        } else {
             binding.emptyStateLayout.visibility = View.GONE
             binding.recyclerView.visibility = View.VISIBLE
         }
@@ -150,9 +170,9 @@ class ActionListFragment : Fragment() {
     }
 
     companion object {
-        val EVENTS_PER_PAGE: Int = 20
+        const val EVENTS_PER_PAGE: Int = 20
         const val IS_CONTRIB = "isContrib"
-        fun newInstance(isContrib: Boolean , ismine: Boolean): ActionListFragment {
+        fun newInstance(isContrib: Boolean, ismine: Boolean): ActionListFragment {
             val fragment = ActionListFragment()
             val args = Bundle()
             args.putBoolean(IS_CONTRIB, isContrib)
