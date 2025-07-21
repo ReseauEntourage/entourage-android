ef829132a7b299ca541b993f079554769383d2bf
François Pellissier <francois.pellissier@gmail.com>
2025-07-21 09:03:39 +0200

    chore: removing OSS plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
-buildscript {
-    dependencies {
-        classpath("com.google.android.gms:oss-licenses-plugin:0.10.6")
-    }
-}
-
 plugins {
     id("com.android.application") version "8.9.3" apply false
     id("com.google.gms.google-services") version "4.4.3" apply false
