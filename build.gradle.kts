3f0b7092fd8c60833a65cb4a9298b6ea11421e58
François Pellissier <francois.pellissier@gmail.com>
2025-12-05 18:51:28 +0100

    chore(deps) : bump aboutlibraries to 12.2.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,5 +8,5 @@ plugins {
     kotlin("android") version "2.1.0" apply false
     //id("com.google.firebase.firebase-perf") version "1.4.2" apply false // Performance Monitoring plugin
     id("androidx.navigation.safeargs") version "2.9.1" apply false
-    id("com.mikepenz.aboutlibraries.plugin") version "11.2.0" apply false
+    id("com.mikepenz.aboutlibraries.plugin") version "12.2.4" apply false
 }
