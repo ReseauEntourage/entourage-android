f38c2a7d77bd7c41917231205e5b62c5d489e646
François Pellissier <francois.pellissier@gmail.com>
2025-12-06 10:50:20 +0100

    chore(deps) : bump aboutlibraries to 13.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,9 +4,9 @@ plugins {
     id("com.android.application") version "8.13.1" apply false
     id("com.google.gms.google-services") version "4.4.3" apply false
     id("com.google.firebase.crashlytics") version "3.0.4" apply false
-    kotlin("plugin.serialization") version "2.1.0" apply false   // <— AJOUTE ÇA
+    kotlin("plugin.serialization") version "2.1.0" apply false   
     kotlin("android") version "2.1.0" apply false
     //id("com.google.firebase.firebase-perf") version "1.4.2" apply false // Performance Monitoring plugin
     id("androidx.navigation.safeargs") version "2.9.1" apply false
-    id("com.mikepenz.aboutlibraries.plugin") version "12.2.4" apply false
+    id("com.mikepenz.aboutlibraries.plugin.android") version "13.1.0" apply false
 }
