ee9f219bdad895e36e1d7e520e7ef69f26d3824a
François Pellissier <francois.pellissier@gmail.com>
2025-12-07 09:42:27 +0100

    chore: migration to version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("com.android.application") version "8.13.1" apply false
     id("com.google.gms.google-services") version "4.4.3" apply false
     id("com.google.firebase.crashlytics") version "3.0.4" apply false
-    kotlin("plugin.serialization") version "2.1.0" apply false   
+    kotlin("plugin.serialization") version "2.1.0" apply false
     kotlin("android") version "2.1.0" apply false
     //id("com.google.firebase.firebase-perf") version "1.4.2" apply false // Performance Monitoring plugin
     id("androidx.navigation.safeargs") version "2.9.1" apply false
