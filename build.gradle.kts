3151e18461d2fac1a8bf2687ad7c337179546449
Clément Perrousset <clement.perrousset@gmail.com>
2025-12-10 15:50:29 +0100

    chore(new lib system ) : switch to new way for lib , with toml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 plugins {
     id("com.android.application") version "8.13.1" apply false
     id("com.google.gms.google-services") version "4.4.3" apply false
     id("com.google.firebase.crashlytics") version "3.0.4" apply false
-    kotlin("plugin.serialization") version "2.1.0" apply false   // <— AJOUTE ÇA
+    kotlin("plugin.serialization") version "2.1.0" apply false
     kotlin("android") version "2.1.0" apply false
-    //id("com.google.firebase.firebase-perf") version "1.4.2" apply false // Performance Monitoring plugin
+    kotlin("kapt") version "2.1.0" apply false
     id("androidx.navigation.safeargs") version "2.9.1" apply false
 }
