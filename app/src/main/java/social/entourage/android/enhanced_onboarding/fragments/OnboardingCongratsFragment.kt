bba6d7bf42fad4fcbe8db9e06ce50090ecef99a9
clément perrousset <clement.perrousset@gmail.com>
2026-01-16 17:31:59 +0100

    fix(OnboardingCongratsFragment) : contribution

--- app/src/main/java/social/entourage/android/enhanced_onboarding/fragments/OnboardingCongratsFragment.kt
+++ app/src/main/java/social/entourage/android/enhanced_onboarding/fragments/OnboardingCongratsFragment.kt
@@ -91,12 +91,15 @@ class OnboardingCongratsFragment: Fragment() {
         var titleRes = R.string.onboarding_congrats_title
         var contentRes = R.string.onboarding_congrats_content
         var buttonTextRes = R.string.onboarding_congrats_leave
-        // Condition par défaut : si la liste est vide ou que la préférence n'est pas "contribution"
+
+        // Condition par défaut : si la liste est vide ou que la préférence est "contribution"
         if (categoriesList.isEmpty() || EnhancedOnboarding.preference == "contribution") {
             binding.tvTitle.setText(titleRes)
             binding.tvDescription.setText(contentRes)
             binding.buttonStart.setText(buttonTextRes)
-            category = "event"
+
+            // CORRECTION ICI : On définit la catégorie sur "contribution" au lieu de "event"
+            category = "contribution"
         }
         // Priorité 1 : Présence de "both_actions" dans la liste
         else if (categoriesList.contains("both_actions")) {
@@ -134,7 +137,7 @@ class OnboardingCongratsFragment: Fragment() {
             binding.tvDescription.setText(contentRes)
             binding.buttonStart.setText(buttonTextRes)
             category = "resources"
-         }
+        }
         // Priorité 4 : Présence de "neighborhoods" dans la liste
         else if (categoriesList.contains("neighborhoods")) {
             titleRes = R.string.onboarding_ready_action_title
@@ -146,4 +149,5 @@ class OnboardingCongratsFragment: Fragment() {
             category = "neighborhoods"
         }
     }
+
 }
