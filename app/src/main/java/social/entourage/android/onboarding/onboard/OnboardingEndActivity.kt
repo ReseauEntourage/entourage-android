8b160797677eec6282c42c99925390c20e8e3d9b
François Pellissier <francois.pellissier@gmail.com>
2026-07-18 08:07:09 +0200

    fix: EN-9409 fix button for all navigation modes in final Onboarding screen

--- app/src/main/java/social/entourage/android/onboarding/onboard/OnboardingEndActivity.kt
+++ app/src/main/java/social/entourage/android/onboarding/onboard/OnboardingEndActivity.kt
@@ -2,19 +2,32 @@ package social.entourage.android.onboarding.onboard
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import social.entourage.android.EntourageApplication
-import social.entourage.android.MainActivity
-import social.entourage.android.api.OnboardingAPI
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 import social.entourage.android.databinding.ActivityOnboardingEndBinding
 import social.entourage.android.notifications.NotificationDemandActivity
 import social.entourage.android.tools.log.AnalyticsEvents
 
 class OnboardingEndActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         val binding = ActivityOnboardingEndBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, windowInsets ->
+            val systemBars = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.updatePadding(
+                left = systemBars.left,
+                top = systemBars.top,
+                right = systemBars.right,
+                bottom = systemBars.bottom
+            )
+            windowInsets
+        }
 
         binding.uiOnboardingBtEnd.setOnClickListener {
             AnalyticsEvents.logEvent(AnalyticsEvents.Clic__Start__Onboarding__Confirmation)
