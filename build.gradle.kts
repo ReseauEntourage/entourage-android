faf198325c8fa080836f5e94624baae5cb52123f
François Pellissier <francois.pellissier@gmail.com>
2026-07-28 18:43:42 +0200

    chore: upgrade AGP to 9.3.1 and refactor build configuration
    - update `android-application` plugin version to 9.3.1 in `libs.versions.toml`
    - enable `android.newDsl` in `gradle.properties`
    - remove redundant `kotlin-android` plugin aliases and explicit `KotlinCompile` task configurations
    - comment out `aaptOptions.cruncherEnabled` in the debug build configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
     alias(libs.plugins.google.services) apply false
     alias(libs.plugins.firebase.crashlytics) apply false
     alias(libs.plugins.kotlin.serialization) apply false
-    alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.navigation.safeargs) apply false
     alias(libs.plugins.aboutlibraries) apply false
     alias(libs.plugins.ksp) apply false
