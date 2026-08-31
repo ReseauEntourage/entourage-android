ecd6656a1ebf12ae66374bda2c3c536debe6a80b
François Pellissier <francois.pellissier@gmail.com>
2026-08-31 12:00:18 +0200

    chore: [Tier 2] remove always-left Elvis operators

--- app/src/main/java/social/entourage/android/enhanced_onboarding/fragments/OnboardingActionWishesFragment.kt
+++ app/src/main/java/social/entourage/android/enhanced_onboarding/fragments/OnboardingActionWishesFragment.kt
@@ -103,8 +103,8 @@ class OnboardingActionWishesFragment : Fragment() {
         }
 
         // On récupère les listes existantes pour éviter les null check répétitifs
-        val userOrientations = user.orientations ?: emptyList()
-        val userInvolvements = user.involvements ?: emptyList()
+        val userOrientations = user.orientations
+        val userInvolvements = user.involvements
 
         android.util.Log.d("EN9530_DEBUG", "loadAndSendActionWishes: goal=${user.goal} isUserTypeAlone=${user.isUserTypeAlone} " +
                 "EnhancedOnboarding.preference=${EnhancedOnboarding.preference} isIsolatedPersonMode=${isIsolatedPersonMode()} " +
