736876cc08311041fa7876b85154547868114c94
clem <clement.perrousset@gmail.com>
2026-09-15 14:38:36 +0200

    chore(onboarding): retirer les logs de debug EN9530_DEBUG
    EN-9530

--- app/src/main/java/social/entourage/android/enhanced_onboarding/EnhancedOnboarding.kt
+++ app/src/main/java/social/entourage/android/enhanced_onboarding/EnhancedOnboarding.kt
@@ -44,9 +44,6 @@ class EnhancedOnboarding : BaseActivity() {
         viewModel = ViewModelProvider(this).get(OnboardingViewModel::class.java)
         viewModel.user = EntourageApplication.me(this)
 
-        android.util.Log.d("EN9530_DEBUG", "EnhancedOnboarding.onCreate: user.goal=${viewModel.user?.goal} " +
-                "isUserTypeAlone=${viewModel.user?.isUserTypeAlone} EnhancedOnboarding.preference=$preference")
-
         val userGoal = viewModel.user?.goal
         val isAssoRole = viewModel.user?.partner != null && (viewModel.user?.roles?.contains("Association") == true || viewModel.user?.roles?.contains("Équipe Entourage") == true)
 
@@ -225,8 +222,6 @@ class EnhancedOnboarding : BaseActivity() {
                     else -> OnboardingNavigation.Home
                 }
             }
-
-            android.util.Log.d("EN9530_DEBUG", "handleOnboardingShouldQuit: selectedCategory=${viewModel.selectedCategory} -> navigation=$navigation")
 
             val intent = Intent(this, MainActivity::class.java)
             intent.putExtra("extra_onboarding_navigation", navigation)
